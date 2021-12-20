2716553df708c998c213fe840ccd52f692002ee4
20Alireza20 <rostamialireza42@outlook.com>
2021-12-20 16:04:09 +0330

    mmd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as Icon from 'react-bootstrap-icons';
 
 class App extends Component {
   render() {
@@ -10,7 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>    
-        <p>mmd</p> 
+        <p>ممد باقر رفته بود باشگاه</p> 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import './font/IRANSansWeb_Medium.ttf'
+import * as Icon from 'react-bootstrap-icons';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
